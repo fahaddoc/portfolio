289b5b05d5a4f72e8c54ee9f0d6bc3aa3d3d0007
Shah Fahad <fahaddoc600@gmail.com>
2025-03-11 10:39:39 +0500

    update the image container

--- src/components/projects/Project1.vue
+++ src/components/projects/Project1.vue
@@ -1,53 +1,106 @@
 <script setup lang="ts">
 
 defineProps<{
-    imageLeft: string,
-    imageRight: string,
-    description:string
+  imageLeft: string,
+  imageRight: string,
+  description: string
 }>();
 
 </script>
 
 <template>
-    <div class="col">
-        <div class="row">
-            <div class="left-box">
-                <img alt="" :src="imageLeft" />
-            </div>
-            <div class="right-box">
-                <img alt="" :src="imageRight" />
-            </div>
+  <div class="col">
+    <div class="row">
+      <div class="left-box">
+        <div class="cont lft">
+          <img alt="" :src="imageLeft" height="300"/>
+          <div class="para">
+            <p>Flutter app which run in various platforms, Flutter app which run in various platforms,
+              Flutter app which run in various platforms,Flutter app which run in various platforms,Flutter app which run
+              in various platforms,Flutter app which run in various
+              platforms.
+            </p>
+          </div>
         </div>
-        <div class="description">
-            {{description}}
+      </div>
+      <div class="right-box">
+        <div class="cont rt">
+          <img alt="" :src="imageRight" height="300"/>
+          <div class="para">
+            <p>Flutter app which run in various platforms, Flutter app which run in various platforms,
+              Flutter app which run in various platforms,Flutter app which run in various platforms,Flutter app which run
+              in various platforms,Flutter app which run in various
+              platforms.
+            </p>
+          </div>
         </div>
+      </div>
     </div>
+    <div class="description">
+      {{ description }}
+    </div>
+  </div>
 </template>
 
 <style scoped>
-.col{
-    display: flex;
-    flex-direction: column;
-    width: 100%;
-    background-color: yellow;
-}
-.col > .row{
-    display: flex;
-    justify-content: center;
-    width: 100%;
-    background-color: red;
-}
-.row > .left-box{
-    display: flex;
-    flex: 1;
-    width: 100%;
-    background-color: white;
+.col {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
 }
 
-.row > .right-box{
-    display: flex;
-    flex: 1;
-    width: 100%;
-    background-color: #ff926e;
+.col > .row {
+  display: flex;
+  justify-content: center;
+  width: 100%;
 }
+
+.row > .left-box {
+  display: flex;
+  justify-content: center;
+  padding: 0.5rem 0;
+  flex: 1;
+  width: 100%;
+  background-color: white;
+}
+
+.row > .right-box {
+  display: flex;
+  justify-content: center;
+  flex: 1;
+  width: 100%;
+  background-color: #ff926e;
+}
+
+.cont{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 80%;
+  margin: 1rem;
+}
+
+.cont.lft{
+  border: 1px solid black;
+  border-radius: 1rem;
+}
+
+.cont.rt{
+  border: 1px solid white;
+  border-radius: 1rem;
+}
+
+.cont > img{
+  border-radius: 1rem;
+}
+.cont > .para {
+  display: flex;
+  width: 100%;
+  color: #222222;
+  justify-content: center;
+  margin: 1rem;
+  padding: 0 0.5rem;
+}
+
+
 </style>
--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,10 +1,23 @@
 <script setup lang="ts">
 import Project1 from '@/components/projects/Project1.vue';
+import imageLeft from '@/assets/projects/project1/digitalhire_1.png';
+import imageRight from '@/assets/projects/project1/digitalhire_2.webp';
 </script>
 
 <template>
     <div class="heading">
         My Work
     </div>
-    <Project1 imageRight="@/assets/projects/project1/digitalhire_1.png" imageLeft="@/assets/projects/project1/digitalhire_1.png" description="shhs"/>
+    <Project1 :imageRight="imageRight" :imageLeft="imageLeft" description="shhs"/>
 </template>
+
+<style scoped>
+.heading{
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  justify-items: center;
+  font-size: 32px;
+  background-color: gray;
+}
+</style>
